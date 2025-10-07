f16d48f85f6e7085bef548cd325c09985971446a
escoladedesenhos6b-byte <escoladedesenhos6b@gmail.com>
2025-10-07 19:18:27 -0300

    js
    novos comandos

---
+++ index.js
@@ -0,0 +1,8 @@
+const div1 = document.querySelectorAll('.stage-active')
+const divs2 = document.querySelectorAll('.stage')
+let indiceatual = 0;
+
+function tpc () {
+    const botaogira = getElementeById('gira')
+    
+}
